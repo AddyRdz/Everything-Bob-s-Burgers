694f573eff3380aae3eafffa738d54adfb942134
Addy Rodriguez <rdz.33.a@gmail.com>
2022-02-11 20:38:33 -0600

    got characters in their own container

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -3,22 +3,24 @@ function CharacterList(props){
     console.log("This is the character list",props.characterList)
     return(
         <div>
-        <p>This is the character list page!</p>
+            <p>This is the character list page!</p>
+            <section className="container">
 
-        {props.characterList.map( (bobCharacters, index) =>{
-            return(
-                <div className="card" key={index}>
-                <div className="card-image">
-                    <img src={bobCharacters.image}/>
-                    <ul>
-                    <li>Name:{bobCharacters.name}</li>
-                    <li>First Appearance:{bobCharacters.firstEpisode}</li>
-                    <li>Voiced By:{bobCharacters.voicedBy}</li>
-                    </ul>
-                </div>    
-                </div>    
-            )
-        })}
+                {props.characterList.map( (bobCharacters, index) =>{
+                    return(
+                        <div className="card" key={index}>
+                        <div className="card-image">
+                            <img src={bobCharacters.image}/>
+                            <ul>
+                            <li>Name:{bobCharacters.name}</li>
+                            <li>First Appearance:{bobCharacters.firstEpisode}</li>
+                            <li>Voiced By:{bobCharacters.voicedBy}</li>
+                            </ul>
+                        </div>    
+                        </div>    
+                    )
+                })}
+         </section>
         </div>
     )
 }
